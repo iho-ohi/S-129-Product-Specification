b6f1b58d43b973c2768870ee67dadd91c5b09f32
Jason Rhee <j.rhee@omcinternational.com>
2026-06-04 16:18:27 +1000

    First-cut Lua-version of S-129 PC as put together by OSI Maritime.

---
+++ PC/S129_Portrayal/Rules/UnderKeelClearanceAlmostNonNavigableArea.lua
@@ -0,0 +1,5 @@
+function UnderKeelClearanceAlmostNonNavigableArea (feature, featurePortrayal, contextParameters)
+    featurePortrayal:AddInstructions('ViewingGroup:29020;DrawingPriority:6;DisplayPlane:UnderRADAR')
+    featurePortrayal:AddInstructions ("ColorFill:GOLDN,0.5")
+
+end
---
+++ PC/S129_Portrayal/Rules/UnderKeelClearanceControlPoint.lua
@@ -0,0 +1,4 @@
+function UnderKeelClearanceControlPoint(feature, featurePortrayal, contextParameters)
+    featurePortrayal:AddInstructions('ViewingGroup:29050;DrawingPriority:21;DisplayPlane:UnderRADAR')
+    featurePortrayal:AddInstructions("LocalOffset:0,0;PointInstruction:UKCCONPT")
+end
